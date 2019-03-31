d1db09ba006d843564e76801ab5673f1ac6a3829
WikyPassu <47321306+WikyPassu@users.noreply.github.com>
2019-03-31 18:32:01 -0300

    Add files via upload

---
+++ TP1 - Copy/Menu.h
@@ -0,0 +1,87 @@
+/** \brief
+ *
+ * \param est_x int
+ * \param est_y int
+ * \param x int
+ * \param y int
+ * \return void
+ *
+ */
+void mostrarMenu(int est_x, int est_y, int x, int y);
+
+/** \brief
+ *
+ * \return void
+ *
+ */
+void elegirOpcion();
+
+/** \brief
+ *
+ * \return int
+ *
+ */
+int ingresarNumero();
+
+/** \brief
+ *
+ * \param est_a int
+ * \param est_b int
+ * \return int
+ *
+ */
+int comprobarEstados(int est_a, int est_b);
+
+/** \brief
+ *
+ * \param indet int
+ * \param n1 int
+ * \param n2 int
+ * \return void
+ *
+ */
+void mostrarOperaciones(int indet, int n1, int n2);
+
+/** \brief
+ *
+ * \param n1 int
+ * \param n2 int
+ * \return int
+ *
+ */
+int sumar(int n1,int n2);
+
+/** \brief
+ *
+ * \param n1 int
+ * \param n2 int
+ * \return int
+ *
+ */
+int restar(int n1,int n2);
+
+/** \brief
+ *
+ * \param n1 int
+ * \param n2 int
+ * \return int
+ *
+ */
+int multiplicar(int n1,int n2);
+
+/** \brief
+ *
+ * \param n1 int
+ * \param n2 int
+ * \return float
+ *
+ */
+float dividir(int n1,int n2);
+
+/** \brief
+ *
+ * \param n int
+ * \return int
+ *
+ */
+int sacarFactorial(int n);
---
+++ TP1/Menu.h
@@ -0,0 +1,20 @@
+
+void mostrarMenu(int est_x, int est_y, int x, int y);
+
+void elegirOpcion();
+
+int ingresarNumero();
+
+int comprobarEstados(int est_a, int est_b);
+
+void mostrarOperaciones(int indet, int n1, int n2);
+
+int sumar(int n1,int n2);
+
+int restar(int n1,int n2);
+
+int multiplicar(int n1,int n2);
+
+float dividir(int n1,int n2);
+
+int sacarFactorial(int n);
